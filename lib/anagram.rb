c4ed1fcf1d56cb3cc4bca19452a931af81e5fa99
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-15 18:24:34 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match (candidates)
+    matches = []
+    original_letters = word.chars.sort
+    candidates.each{|candidate| 
+      cand_letters = candidate.chars.sort
+      proceed = true
+      index = 0
+      while proceed
+        cand_letters.
+    }
+    
+    matches
+  end
+end
